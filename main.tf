fa2c40bc69969d7f2f69d871235c5d9ea88a5ce1
joseph <53045966+jefrnc@users.noreply.github.com>
2022-12-06 17:29:16 -0300

    cchanges

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
-variable "aws_access_key" {
-  description = "Access-key-for-AWS"
-  default = "no_access_key_value_found"
-}
- 
-variable "aws_secret_key" {
-  description = "Secret-key-for-AWS"
-  default = "no_secret_key_value_found"
-}
- 
 provider "aws" {
 	region = "eu-west-3"
-	access_key = var.aws_access_key
-	secret_key = var.aws_secret_key
 }
  
 resource "aws_instance" "example" {
